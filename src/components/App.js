d3918b1b7b6d6ab3caebe1af7ad6a36394c8346d
Chen Gazit <gazit.chen@gmail.com>
2019-09-07 19:19:45 +0300

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import LoginPage from '../pages/LoginPage';
+import Main from '../pages/Main';
+
+const App = () => {
+    return( 
+        <BrowserRouter>
+            <Route path="/" component={Main} />
+            <Switch>
+                <Route path="/login" component={LoginPage} />
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+var outputPath = path.normalize("C:\dist\digitalCV");
+var TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: {
+        bundle: './src/index.js',
+    },
+    devServer: {
+        contentBase: outputPath,
+        compress: true,
+        port: 3000,
+        historyApiFallback: true
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({
+            extractComments: true
+        })],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, '/dist')},
+        module:{
+            rules:[
+            {
+                test: /\.js$/, // include .js files
+                enforce: "pre", // preload the jshint loader
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                exclude: __dirname + '/node_modules',
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader', options: {
+                            url: true
+                        }
+                    },
+
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+                exclude: /node_modules/,
+                use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
+            },
+            {
+                test: /\.(ttf|eot|woff|woff2)$/,
+                exclude: __dirname + '/node_modules',
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        outputPath: 'fonts',
+                        name: "[name].[ext]",
+                        publicPath: 'fonts'
+                    },
+                }
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            }]
+        },
+        plugins:[
+            new HtmlWebpackPlugin({
+                filename: "index.html",
+                template: path.join(__dirname, "src", "index.html")
+            })
+        ],
+    stats: {
+        colors: true
+    }
+}
